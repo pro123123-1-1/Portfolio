369ffb24f8f779d9e234f0e4f96e74e857f133e0
Haifahasher <Haifahasher@gmail.com>
2026-01-29 09:57:02 +0300

    Fix: Remove duplicate imports causing build failure

--- frontend-react/src/pages/LoginPage.jsx
+++ frontend-react/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import Header from '../components/Header'
-import Header from '../components/Header'
+
 import Footer from '../components/Footer'
 import API_BASE_URL from '../apiConfig'
 
--- frontend-react/src/pages/SignupPage.jsx
+++ frontend-react/src/pages/SignupPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useNavigate, Link, useSearchParams } from 'react-router-dom'
 import Header from '../components/Header'
-import Header from '../components/Header'
+
 import Footer from '../components/Footer'
 import API_BASE_URL from '../apiConfig'
 
